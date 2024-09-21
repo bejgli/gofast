fce17738bf1f54341411499a1cbb0df37fce3b4a
bejgli <laszlotorhosi01@gmail.com>
2024-09-21 13:32:06 +0200

    Refactor the main sorting loop Proof of concept was created with everything nested in a main function.

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"encoding/json"
 	"log"
 	"os"
-	"regexp"
 
 	"github.com/bejgli/gofast/sorter"
 	"github.com/fsnotify/fsnotify"
 )
 
+var CONFIGPATH = "patterns.json"
+
 func main() {
 	log.Print("Reading configuration file.")
-	configFile, err := os.ReadFile("patterns.json")
+	configFile, err := os.ReadFile(CONFIGPATH)
 	if err != nil {
 		log.Fatal("Couldn't open config file.")
 	}
 
 	var conf sorter.Config
-
 	err = json.Unmarshal(configFile, &conf)
 	if err != nil {
 		log.Fatal(err)
@@ -31,52 +31,44 @@ func main() {
 	}
 	defer watcher.Close()
 
-	// Start listening for events.
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				log.Println("event:", event)
-				if event.Has(fsnotify.Create) {
-					files, err := os.ReadDir("./" + conf.Source)
-					if err != nil {
-						log.Fatal("Cannot read source Dir", conf.Source)
-					}
+	go watchLoop(watcher, conf)
 
-					log.Println("Sorting files.")
-					for _, f := range files {
-						if f.IsDir() {
-							continue
-						}
-
-						for _, r := range conf.Rules {
-							if regexp.MustCompile(r.Pattern).MatchString(f.Name()) {
-								_, err = sorter.MoveFile(conf.Source+"/"+f.Name(), r.Target+"/"+f.Name())
-								if err != nil {
-									log.Fatal(err)
-								}
-							}
-						}
-					}
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	// Add a path.
 	err = watcher.Add(conf.Source)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Block main goroutine forever.
 	<-make(chan struct{})
+}
+
+func watchLoop(watcher *fsnotify.Watcher, conf sorter.Config) {
+	for {
+		select {
+
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			log.Println("event:", event)
+
+			if event.Has(fsnotify.Create) {
+				files, err := os.ReadDir(conf.Source)
+				if err != nil {
+					log.Fatal("Cannot read source Dir", conf.Source)
+				}
+
+				log.Println("Sorting files.")
+				err = sorter.SortFiles(files, conf)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
 }
--- sorter/utils.go
+++ sorter/utils.go
@@ -2,10 +2,12 @@ package sorter
 
 import (
 	"io"
+	"io/fs"
 	"os"
+	"regexp"
 )
 
-func MoveFile(source string, target string) (written int64, err error) {
+func moveFile(source string, target string) (written int64, err error) {
 	sourceFile, err := os.Open(source)
 	if err != nil {
 		return
@@ -28,5 +30,23 @@ func MoveFile(source string, target string) (written int64, err error) {
 		return
 	}
 
+	return
+}
+
+func SortFiles(files []fs.DirEntry, conf Config) (err error) {
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+
+		for _, r := range conf.Rules {
+			if regexp.MustCompile(r.Pattern).MatchString(f.Name()) {
+				_, err = moveFile(conf.Source+"/"+f.Name(), r.Target+"/"+f.Name())
+				if err != nil {
+					return
+				}
+			}
+		}
+	}
 	return
 }
